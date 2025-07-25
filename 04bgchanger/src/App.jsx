fb46e7864ebbfae134cd069180972847f6f7b672
RajanYadav07 <rajanpbh03@gmail.com>
2025-07-25 21:51:51 +0530

    baground changer

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [color,setColor] = useState('bg-white')
+  return (
+    <>
+   <div>
+    <h1>sanu</h1>
+   </div>
+    </>
+  )
+}
+
+export default App
